8a11cfaa29f807ddaf50f900267fdc9b04afb7b8
suchishahp-ux <suchishahp@gmail.com>
2025-10-30 09:16:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+##from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -11,7 +11,8 @@ st.write(
   """
 )
 
-
+cnx= st.connection("snowflake")
+session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
