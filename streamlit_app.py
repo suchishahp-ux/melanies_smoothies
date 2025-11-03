e57646f77ff4e047205bdb35e7a27cb503997d5e
suchishahp-ux <suchishahp@gmail.com>
2025-11-03 10:20:28 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 # Load fruit options from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop();  
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop();  
 
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
